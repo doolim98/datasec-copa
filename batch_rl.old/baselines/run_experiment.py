b696bb3ee4fb87d0a616eccc58648abf4595a598
hjoll6 <doolim98@gmail.com>
2024-12-05 21:39:19 +0900

    Backup

--- batch_rl.old/baselines/run_experiment.py
+++ batch_rl.old/baselines/run_experiment.py
@@ -13,3 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Logged Runner."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from dopamine.discrete_domains import run_experiment
+import gin
+
+
+@gin.configurable
+class LoggedRunner(run_experiment.Runner):
+
+  def run_experiment(self):
+    super(LoggedRunner, self).run_experiment()
+    # Log the replay buffer at the end
+    self._agent.log_final_buffer()
